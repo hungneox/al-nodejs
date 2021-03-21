e2d19dfc52b235d2f72f7e3da5a9c9d72acd255d
Hung Nguyen <hung.nguyen@digia.com>
2021-03-21 13:41:19 +0200

    Setup babel-node

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser';
+import http from 'http';
+import socket from 'socket.io';
+import apiRouter from './api';
+
 const app = express();
 
-const host = "localhost";
-const port = 3000;
+const host = 'localhost';
+const port = process.env.PORT || 8080;
+
+const server = http.Server(app);
+const io = socket(server);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
-const apiRouter = require('./api/router');
+// allow-cors
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(apiRouter);
+
+// catch 404
+app.use((req, res, next) => {
+  res.status(404).send('Page Not Found!');
+});
 
 app.listen(port, () => {
   console.log(`Server is listening at http://${host}:${port}`);
